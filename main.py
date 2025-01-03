83858343b844b2a1f744a41b6c047a742bd7525d
pubdomacc <publicdomainacc@gmail.com>
2025-01-03 20:10:43 +0700

    Update_20250103
    Added buttons to navigate

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 import streamlit as st
 st.write ("Hello world")
+
+#If pressed the state would be True
+if (st.button("Input Data")) :
+
+	st.write("Will Input Data Here")
+
+if (st.button("Go to Dashboard")) :
+	st.write("Will Go to Dashboard page")
