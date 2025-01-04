80b4bae63ce1bd616d5cc21cea46fa0a8edb6d38
pubdomacc <publicdomainacc@gmail.com>
2025-01-04 18:06:03 +0700

    Update main.py
    Change dictionary key, form value saved to dict, warning on submit button

--- main.py
+++ main.py
@@ -16,17 +16,21 @@ st.title("Daily Update Form - Workout")
 duf_data_input = {
 	"date" : None,
 	"weight" : None,
-	"fat%" : None,
-	"water%" : None,
-	"muscle%" : None
+	"fat" : None,
+	"water" : None,
+	"muscle" : None
 }
 
 with st.form(key="duf_workout") :
-	duf_input_Date = st.date_input("What's today's date?")
-	duf_input_weight = st.number_input("Enter your morning weight :")
-	duf_input_fat = st.number_input("Enter your morning fat % :")
-	duf_input_water = st.number_input("Enter your morning water % :")
-	duf_input_muscle = st.number_input("Enter your morning muscle % :")
+	duf_data_input["date"] = st.date_input("What's today's date?")
+	duf_data_input["weight"] = st.number_input("Enter your morning weight :")
+	duf_data_input["fat"] = st.number_input("Enter your morning fat % :")
+	duf_data_input["water"] = st.number_input("Enter your morning water % :")
+	duf_data_input["muscle"] = st.number_input("Enter your morning muscle % :")
 	
 
-	submit_button = st.form_submit_button("Submit")
+	submit_button = st.form_submit_button("Submit Data")
+	if submit_button :
+		#if not all(duf_data_input.values()):
+		if (duf_data_input.values()).contains(None):
+			st.warning("Please fill in all the fields")
