7ca4e010abb164ff4ec9e034a02d64d724650df5
pubdomacc <publicdomainacc@gmail.com>
2025-01-04 17:55:52 +0700

    Update main.py
    Update to try out input form v1

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 st.write ("Hello world")
 
+def mainFunc():
 #If pressed the state would be True
 if (st.button("Input Data")) :
 
@@ -8,3 +9,24 @@ if (st.button("Input Data")) :
 
 if (st.button("Go to Dashboard")) :
 	st.write("Will Go to Dashboard page")
+
+
+st.title("Daily Update Form - Workout")
+
+duf_data_input = {
+	"date" : None,
+	"weight" : None,
+	"fat%" : None,
+	"water%" : None,
+	"muscle%" : None
+}
+
+with st.form(key="duf_workout") :
+	duf_input_Date = st.date_input("What's today's date?")
+	duf_input_weight = st.number_input("Enter your morning weight :")
+	duf_input_fat = st.number_input("Enter your morning fat % :")
+	duf_input_water = st.number_input("Enter your morning water % :")
+	duf_input_muscle = st.number_input("Enter your morning muscle % :")
+	
+
+	submit_button = st.form_submit_button("Submit")
