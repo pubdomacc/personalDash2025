2ad3601d8477620fd36c9a286e6b43d40ba3948b
pubdomacc <publicdomainacc@gmail.com>
2025-01-05 20:41:10 +0700

    Major Update main.py
    Adding separate button to navigate between pages, for testing

--- main.py
+++ main.py
@@ -1,59 +1,40 @@
 import streamlit as st
-st.write ("Hello world")
 
+#global varibles
+
+page_State = None
+
+if "page_State" not in st.session_state:
+	st.session_state.page_State = None
+
+st.logo("images/horizontal_blue.png", icon_image="images/icon_blue.png")
+
+
+#Functions
 def mainFunc():
-	#If pressed the state would be True
-	if (st.button("Input Data")) :
+	return 0;
 
-		st.write("Will Input Data Here")
+#To format and control main page
 
-	if (st.button("Go to Dashboard")) :
-		st.write("Will Go to Dashboard page")
+st.header ("WELCOME")
 
 
-st.title("Daily Update Form - Workout")
+#st.Page
+#(page, *, title=None, icon=None, url_path=None, default=False)
+inputForm = st.Page("DataInput/InputForm.py",title="Daily Input Form") 
+Dashboard = st.Page("DataDisplay/Dashboard.py",title="Daily Dashboard")
 
-duf_data_input = {
-	"date" : None,
-	"weight" : None,
-	"fat" : None,
-	"water" : None,
-	"muscle" : None,
-	"exercise_status" : None,
-	"exercise_type" : None
-}
+#If pressed the state would be True
+if (st.button("Input Data")) :
+	st.session_state.page_State = "InputForm"
+	st.write("Will Input Data Here")
+	#will call input data form page here
+	pg = st.navigation ([inputForm])
 
-with st.form(key="duf_workout") :
-	duf_data_input["date"] = st.date_input("What's today's date?", max_value = "today", format="YYYY-MM-DD")
-	duf_data_input["weight"] = st.number_input("Enter your morning weight :")
-	duf_data_input["fat"] = st.number_input("Enter your morning fat % :")
-	duf_data_input["water"] = st.number_input("Enter your morning water % :")
-	duf_data_input["muscle"] = st.number_input("Enter your morning muscle % :")
-	duf_data_input["exercise_status"] = st.radio(
-	    "Did you Workout today?",
-	    [":rainbow[YES!]", "Nope"],
-	    index=None,
-		)
-	duf_data_input["exercise_type"] = st.multiselect(
-	    "Choose exercises you've done today",
-	    ["Treadmill/Cardio", "Morning Walk", "Body Weight", "Yoga", "Weights exercise"],
-	    ["Morning Walk", "Weights exercise"],
-		)	
+if (st.button("Go to Dashboard")) :
+	st.session_state.page_State = "Dashboard"
+	st.write("Will Go to Dashboard page")
+	#will call dashboard data here
+	pg = st.navigation([Dashboard])
 
-
-
-	submit_button = st.form_submit_button("Submit Data")
-	if submit_button :
-		if not all(duf_data_input.values()):
-		#if (duf_data_input.values()).contains(None):
-			st.warning("Please fill in all the fields")
-		else : 
-			st.success ("Thank you for today's input!")
-			st.balloons()
-			st.write("#### Info :")
-			for (key,value) in duf_data_input.items() :
-				st.write(key, " : ", value)
-			st.write("#### multiselect output test :")
-			#for i in options :
-			#	st.write(i, " ")
-			#st.write("#### radio output test : ", exercise)
+pg.run()
